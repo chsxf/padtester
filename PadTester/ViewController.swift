d765a43205ba5665c0c0bb36b16a69277c385dda
Christophe SAUVEUR <christophe@cheeseburgames.com>
2020-11-22 14:58:39 +0100

    Initial Commit

---
+++ PadTester/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PadTester
+//
+//  Created by Christophe on 22/11/2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
